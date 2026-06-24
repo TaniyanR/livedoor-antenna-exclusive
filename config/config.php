183be38c66dec65b8ccc284d7b7fb2bbcf0d8667
TaniyanR <toolhouse@24.am>
2026-06-24 19:29:27 +0900

    Configure XAMPP auto setup

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'livedoor_antenna',
+        'user' => 'root',
+        'pass' => '',
+    ],
+    'auto_setup' => true,
+    'admin' => [
+        'username' => 'admin',
+        'password' => 'admin',
+    ],
+    'settings' => [
+        'site_name' => 'livedoorアンテナ',
+        'site_description' => 'livedoorアンテナ',
+    ],
+];
